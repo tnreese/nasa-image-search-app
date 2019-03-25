926bef37a01ca5a5adc41579fa571313648c2dfa
tnreese <tala.reese99@gmail.com>
2019-03-24 21:58:43 -0400

    final

--- thumbpicture.py
+++ thumbpicture.py
@@ -2,45 +2,55 @@ import urllib.request
 import json
 import webbrowser
      
-searchTerm = input("search:")
-apiurl = 'https://images-api.nasa.gov/search?q='+searchTerm
+#searchTerm = input("search:")
+#apiurl = 'https://images-api.nasa.gov/search?q='+searchTerm
 # call the api
 
-apiurlobj = urllib.request.urlopen(apiurl)
+#apiurlobj = urllib.request.urlopen(apiurl)
 # read JSON file
-apiread = apiurlobj.read()
+#apiread = apiurlobj.read()
 # convert JSON to Python so it is readable
-transapi = json.loads(apiread.decode('utf-8'))
+#transapi = json.loads(apiread.decode('utf-8'))
 
 #print (transapi['collection']['items'][0]['links'][0]['href'])
 #thumbnail = (transapi['collection']['items'][0]['links'][0]['href'])
 #webbrowser.open(picture)
 #title = (transapi['collection']['items'][0]['data'][0]['title'])
 #print (title)
-def readAPI(url):
+def readAPI():
+        searchTerm = input("search:")
+        apiurl = 'https://images-api.nasa.gov/search?q='+searchTerm
         # call the api
-        apiurlobj = urllib.request.urlopen(url)
+        apiurlobj = urllib.request.urlopen(apiurl)
         # read JSON file
         apiread = apiurlobj.read()
         # convert JSON to Python so it is readable
         transapi = json.loads(apiread.decode('utf-8'))
         return(transapi)
 
-def getMetaData():
-        print(transapi['collection']['metadata'])
+#def getMetaData():
+        #print(transapi['collection']['metadata'])
 def resultScreen():
-        getMetaData()
+        transapi = readAPI()
+        f= open("secondpage.html","w")
+        metaData=0
         for key in transapi['collection']['items']:
-                if 'href' in key['links'][0]:
-                        print(key['data'][0]['title'])
-                        webbrowser.open(key['links'][0]['href'])
-                        print(key['data'][0]['description'])
+                if 'links' in key:
+                        try:
+                                if 'href' in key['links'][0]:
+                                        f.write('<p>'+ key["data"][0]["title"]+ '</p>\n')
+                                        f.write('<p><img src="'+ key["links"][0]["href"]+ '" alt=\"\" width=\"300\" height=\"168\" /></p>\n')
+                                        f.write("<p>"+ key['data'][0]['description']+ "</p>\n")
+                                        metaData=metaData+1
+                        except: 
+                                print("missing data")
+        f.close()
+        
         return;
+
 resultScreen()
-pictureLink = transapi['collection']['items'][0]['href']
-pictureList = readAPI(pictureLink)
-smallPicture = pictureList[3]
-print (smallPicture)
+
+
 
 
 
